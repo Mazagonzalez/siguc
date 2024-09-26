f5f0d3a12a4d7568d7b643cceb36a1d9a4b094c6
Cesar <cesarceveriche7@gmail.com>
2024-09-26 08:32:00 -0500

    Se implemento la api en el dashboard y se agrego un filtro

--- resources/views/livewire/user/dashboard-live.blade.php
+++ resources/views/livewire/user/dashboard-live.blade.php
@@ -1,3 +1,27 @@
 <div>
-    Hola este solo es usuario
+    <div>
+        Hola este solo es usuario
+    </div>
+    <div>
+        <input type="text" wire:model="orderId" placeholder="Ingrese el ID de la orden">
+        <button type="text" wire:click='filterOrders'> Buscar </button>
+
+    </div>
+    <div>
+        @foreach($orders as $order)
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">Id #{{ $order['id'] }}</h5>
+                    <p class="card-text">
+                        <strong>Orden:</strong> {{ $order['order_number'] }}<br>
+                        <strong>Cliente:</strong> {{ $order['target_customer'] }}<br>
+                        <strong>Peso Total:</strong> {{ $order['total_weight'] }} kg<br>
+                        <strong>Dirección:</strong> {{ $order['address'] }}<br>
+                        <strong>Contenedor:</strong> {{ $order['container'] }}
+                    </p>
+                    <br>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
