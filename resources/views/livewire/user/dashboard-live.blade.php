f191d0b335847f33d009924372471b78634703bd
Carlos Maza <mazagonzalez61@gmail.com>
2024-10-06 21:50:13 -0500

    - rediseño del dashboard

--- resources/views/livewire/user/dashboard-live.blade.php
+++ resources/views/livewire/user/dashboard-live.blade.php
@@ -1,44 +1,131 @@
 <div class="gap-5 screen-default col">
-    <div class="gap-4 col lg:flex-row">
-        <div class="grid w-[60%] grid-cols-4 gap-4">
-            <div class="items-center gap-3 py-10 col rounded-3xl card-theme">
-                <div class="bg-blue-500 rounded-md size-12 center-content">
-                    <x-icons.message-check class="stroke-white size-8" />
+    <div class="gap-5 row">
+        <div class="w-[65%] gap-3 col">
+            {{-- Total de solicitudes por estado --}}
+            <div class="grid grid-cols-4 px-3 py-5 divide-x-theme card-theme">
+                <div class="gap-2 center-content">
+                    <div class="bg-blue-100 rounded-full center-content size-10">
+                        <x-icons.message-check class="stroke-blue-500 size-5" />
+                    </div>
+
+                    <div class="col">
+                        <span class="text-xl leading-[25px] font-semibold">{{ $request_acepted }}</span>
+                        <div class="font-light col">
+                            <span class="text-[10px] leading-[10px]">Solicitudes</span>
+                            <span class="text-[13px] leading-[13px]">Aceptadas</span>
+                        </div>
+                    </div>
                 </div>
-                <p class="font-semibold text-sm text-center leading-[18px]">Solictudes <br> Aceptadas</p>
-                <p class="text-xl font-semibold">{{ $request_acepted }}</p>
+
+                <div class="gap-2 center-content">
+                    <div class="bg-yellow-100 rounded-full center-content size-10">
+                        <x-icons.clock class="stroke-yellow-500 size-5" />
+                    </div>
+
+                    <div class="col">
+                        <span class="text-xl leading-[25px] font-semibold">{{ $request_pending }}</span>
+                        <div class="font-light col">
+                            <span class="text-[10px] leading-[10px]">Solicitudes</span>
+                            <span class="text-[13px] leading-[13px]">Pendiendtes</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="gap-2 center-content">
+                    <div class="bg-red-100 rounded-full center-content size-10">
+                        <x-icons.x-mark class="stroke-red-500 size-5" />
+                    </div>
+
+                    <div class="col">
+                        <span class="text-xl leading-[25px] font-semibold">{{ $request_rejected }}</span>
+                        <div class="font-light col">
+                            <span class="text-[10px] leading-[10px]">Solicitudes</span>
+                            <span class="text-[13px] leading-[13px]">Finalizadas</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="gap-2 center-content">
+                    <div class="rounded-full bg-emerald-100 center-content size-10">
+                        <x-icons.check-circle class="stroke-emerald-500 size-5" />
+                    </div>
+
+                    <div class="col">
+                        <span class="text-xl leading-[25px] font-semibold">{{ $request_finished }}</span>
+                        <div class="font-light col">
+                            <span class="text-[10px] leading-[10px]">Solicitudes</span>
+                            <span class="text-[13px] leading-[13px]">Finalizadas</span>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div class="items-center gap-3 py-10 col rounded-3xl card-theme">
-                <div class="bg-yellow-400 rounded-md size-12 center-content">
-                    <x-icons.clock class="stroke-white size-8" />
-                </div>
-                <p class="font-semibold text-sm text-center leading-[18px]">Solictudes <br> Pendientes</p>
-                <p class="text-xl font-semibold">{{ $request_pending }}</p>
-            </div>
+            {{-- Total de solicitudes por tipo --}}
+            <div class="grid grid-cols-3 gap-3">
+                <div class="relative gap-6 p-5 overflow-hidden text-white bg-blue-500 col rounded-2xl">
+                    <div class="items-center justify-between row">
+                        <span class="text-sm">Nacionales</span>
 
-            <div class="items-center gap-3 py-10 col rounded-3xl card-theme">
-                <div class="bg-red-500 rounded-md size-12 center-content">
-                    <x-icons.message-cancel class="stroke-white size-8" />
-                </div>
-                <p class="font-semibold text-sm text-center leading-[18px]">Solictudes <br> Rechazadas</p>
-                <p class="text-xl font-semibold">{{ $request_rejected }}</p>
-            </div>
+                        <div class="bg-blue-400 rounded-full size-7 center-content">
+                            <x-icons.flag class="size-5 stroke-white" />
+                        </div>
+                    </div>
 
-            <div class="items-center gap-3 py-10 col rounded-3xl card-theme">
-                <div class="rounded-md bg-emerald-500 size-12 center-content">
-                    <x-icons.check-circle class="stroke-white size-8" />
-                </div>
-                <p class="font-semibold text-sm text-center leading-[18px]">Solictudes <br> Finalizadas</p>
+                    <p class="text-2xl font-semibold">0</p>
 
-                <div class="items-center gap-2 row">
-                    <p class="text-xl font-semibold">{{ $request_finished }}</p>
-                    @livewire('user.ranking-live')
+                    {{-- Figura --}}
+                    <div class="absolute p-4 border-2 border-blue-400 rounded-full -right-14 -bottom-14">
+                        <div class="border-2 border-blue-400 rounded-full size-20"></div>
+                    </div>
+                </div>
+
+                <div class="relative gap-6 p-5 overflow-hidden text-white bg-emerald-500 col rounded-2xl">
+                    <div class="items-center justify-between row">
+                        <span class="text-sm">Extranjeras</span>
+
+                        <div class="rounded-full bg-emerald-400 size-7 center-content">
+                            <x-icons.world class="size-5 stroke-white" />
+                        </div>
+                    </div>
+
+                    <p class="text-2xl font-semibold">0</p>
+
+                    {{-- Figura --}}
+                    <div class="absolute p-4 rotate-45 border-2 border-emerald-400 -right-20 -bottom-20">
+                        <div class="p-4 border-2 border-emerald-400">
+                            <div class="border-2 border-emerald-400 size-16"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="relative gap-6 p-5 overflow-hidden text-white bg-indigo-500 col rounded-2xl">
+                    <div class="items-center justify-between row">
+                        <span class="text-sm">Termoformado</span>
+
+                        <div class="bg-indigo-400 rounded-full size-7 center-content">
+                            <x-icons.unsplash class="size-5 stroke-white" />
+                        </div>
+                    </div>
+
+                    <p class="text-2xl font-semibold">0</p>
+
+                    {{-- Figura --}}
+                    <div class="absolute p-4 border-2 border-indigo-400 rounded-xl -right-16 -bottom-16">
+                        <div class="p-4 border-2 border-indigo-400 rounded-lg">
+                            <div class="border-2 border-indigo-400 rounded-lg size-14"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="w-[40%] py-4 px-6 card-theme col gap-4">
+        <div class="w-[35%] card-theme p-4">
+            @livewire('user.ranking-live')
+        </div>
+    </div>
+
+    <div class="gap-4 col lg:flex-row">
+        {{-- <div class="w-[40%] py-4 px-6 card-theme col gap-4">
             <div class="relative">
                 <input
                     type="text"
@@ -99,7 +186,7 @@
                     @endif
                 @endif
             </div>
-        </div>
+        </div> --}}
     </div>
 
     <div
