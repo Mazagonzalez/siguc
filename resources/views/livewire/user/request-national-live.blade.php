d8bbc3a9da71cd424defa7956a23c58493c02094
Cesar <cesarceveriche7@gmail.com>
2024-10-07 18:54:55 -0500

    se agrego las tablas

--- resources/views/livewire/user/request-national-live.blade.php
+++ resources/views/livewire/user/request-national-live.blade.php
@@ -68,6 +68,56 @@
             <div>
                 @livewire('user.created-requests-national-live', key('request-national'))
             </div>
+            <div
+                x-data="{
+                    typeRequest: 1,
+                    activeClass: 'bg-[#ebecec] dark:bg-[#333333] font-semibold',
+                    inactiveClass: '',
+                    showFilter: false,
+                }" class="relative gap-5 p-8 col card-theme"
+            >
+                <div class="items-center mb-5 row">
+                    <a class="p-4 text-sm rounded-lg cursor-pointer" @click="typeRequest = 1" :class="typeRequest === 1 ? activeClass : inactiveClass">
+                        {{ __('Solicitudes en Proceso')}}
+                    </a>
+                    <a class="p-4 text-sm rounded-lg cursor-pointer" @click="typeRequest = 2" :class="typeRequest === 2 ? activeClass : inactiveClass">
+                        {{ __('Solicitudes por confirmar entrega')}}
+                    </a>
+                    <a class="p-4 text-sm rounded-lg cursor-pointer" @click="typeRequest = 3" :class="typeRequest === 3 ? activeClass : inactiveClass">
+                        {{ __('Historial de Solicitudes')}}
+                    </a>
+                </div>
+
+                <div class="w-full">
+                    <div x-show="typeRequest === 1" x-transition:enter.duration.500ms style="display: none">
+                        @livewire('user.pending-requests-live', key('pending-request-'.auth()->user()->id))
+                    </div>
+
+                    <div x-show="typeRequest === 2" x-transition:enter.duration.500ms style="display: none">
+                        @livewire('user.end-requests-live', key('history-request-'.auth()->user()->id))
+                    </div>
+
+                    <div x-show="typeRequest === 3" x-transition:enter.duration.500ms style="display: none">
+                        @livewire('user.history-requests-live', key('history-request-'.auth()->user()->id))
+                    </div>
+                </div>
+
+            </div>
+
+            @if (session('message'))
+                <div
+                    x-data="{ show: true }"
+                    x-init="setTimeout(() => show = false, 3000)"
+                    x-show="show"
+                    x-transition:leave.duration.400ms
+                    wire:transition
+                    class="fixed z-40 items-center gap-3 px-4 py-2 text-sm font-light text-white rounded-lg bg-emerald-500 row bottom-4 right-4"
+                >
+                    <x-icons.checked class="stroke-white size-5" />
+
+                    {{ session('message') }}
+                </div>
+            @endif
         </div>
     </div>
 </div>
