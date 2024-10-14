c79d90611ec1b2dc06fe5315905661aa32aff1f6
Carlos Maza <mazagonzalez61@gmail.com>
2024-10-13 21:04:33 -0500

    fix: correccion de ranking en el dashboard

--- app/Livewire/User/RankingLive.php
+++ app/Livewire/User/RankingLive.php
@@ -2,39 +2,39 @@
 
 namespace App\Livewire\User;
 
-use App\Models\Request;
+use App\Models\History;
 use Livewire\Component;
-use Illuminate\Support\Facades\DB;
 
 class RankingLive extends Component
 {
-    public $type;
-    public $providers = [];
     public $totalStates;
 
     protected $listeners = ['request' => 'generateRanking'];
 
-    public function generateRanking()
-    {
-        $this->totalStates = Request::where('status', 5)->where('double_order', 0)->count();
-
-        $this->providers = Request::select('provider_id', DB::raw('count(*) as total'))
-            ->where('status', 5)
-            ->where('double_order', 0)
-            ->groupBy('provider_id')
-            ->orderBy('total', 'desc')
-            ->get();
-    }
-
-    public function mount()
-    {
-        $this->generateRanking();
-    }
-
     public function render()
     {
+        $history = History::where('status', 5)->get();
+
+        $providerNames = [];
+
+        foreach ($history as $record) {
+            if ($record->requestNational) {
+                $providerNames[] = $record->requestNational->provider->company_name;
+            }
+
+            if ($record->requestThermoformed) {
+                $providerNames[] = $record->requestThermoformed->provider->company_name;
+            }
+        }
+
+        $providerCounts = array_count_values($providerNames);
+
+        // Calcular el total de estados
+        $this->totalStates = array_sum($providerCounts);
+
         return view('livewire.user.ranking-live', [
             'totalStates' => $this->totalStates,
+            'providerCounts' => $providerCounts,
         ]);
     }
 }
--- resources/views/livewire/user/ranking-live.blade.php
+++ resources/views/livewire/user/ranking-live.blade.php
@@ -14,123 +14,37 @@
                     <th class="th">#</th>
                     <th class="th text-start">Proveedor</th>
                     <th class="th">Total</th>
-                    <th class="th">Flete total</th>
+                    <th class="th">Porcentage</th>
                 </tr>
             </thead>
             <tbody>
-                <tr class="tr">
-                    <td class="td">1</td>
-
-                    <td class="td" style="text-align: start">
-                        <p class="text-sm tooltip tooltip-top" data-tip="LINKARGA">
-                            {{ auth()->user()->short('LINKARGA', 15) }}
-                        </p>
-                    </td>
-
-                    <th class="th">
-                        7
-                    </th>
-
-                    <th class="th">
-                        $ 23.200,00
-                    </th>
-                </tr>
-
-                <tr class="tr">
-                    <td class="td">2</td>
-
-                    <td class="td" style="text-align: start">
-                        <p class="text-sm tooltip tooltip-top" data-tip="COLTANQUES S.A.S">
-                            {{ auth()->user()->short('COLTANQUES S.A.S', 15) }}
-                        </p>
-                    </td>
-
-                    <th class="th">
-                        5
-                    </th>
-
-                    <th class="th">
-                        $ 18.000,00
-                    </th>
-                </tr>
-
-                <tr class="tr">
-                    <td class="td">3</td>
-
-                    <td class="td" style="text-align: start">
-                        <p class="text-sm tooltip tooltip-top" data-tip="TANQUES DEL NOROESTE">
-                            {{ auth()->user()->short('TANQUES DEL NOROESTE', 15) }}
-                        </p>
-                    </td>
-
-                    <th class="th">
-                        3
-                    </th>
-
-                    <th class="th">
-                        $ 15.600,00
-                    </th>
-                </tr>
-
-                <tr class="tr">
-                    <td class="td">4</td>
-
-                    <td class="td" style="text-align: start">
-                        <p class="text-sm tooltip tooltip-top" data-tip="TRANSPORTADORA MULTIGLOBAL S.A.S">
-                            {{ auth()->user()->short('TRANSPORTADORA MULTIGLOBAL S.A.S', 15) }}
-                        </p>
-                    </td>
-
-                    <th class="th">
-                       2
-                    </th>
-
-                    <th class="th">
-                        $ 9.200,00
-                    </th>
-                </tr>
-
-                <tr class="tr" style="border-bottom: 0px">
-                    <td class="td">5</td>
-
-                    <td class="td" style="text-align: start">
-                        <p class="text-sm tooltip tooltip-top" data-tip="COORDINADORA">
-                            {{ auth()->user()->short('COORDINADORA', 15) }}
-                        </p>
-                    </td>
-
-                    <th class="th">
-                        1
-                    </th>
-
-                    <th class="th">
-                        $ 4.500,00
-                    </th>
-                </tr>
-
-               {{--  @forelse ($providers as $index => $provider)
-                    <tr class="tr" @if ($index === 4) style="border-bottom: 0px" @endif>
-                        <td class="td">{{ $index + 1 }}</td>
+                @forelse ($providerCounts as $providerName => $total)
+                    <tr class="tr">
+                        <td class="td">{{ $loop->iteration }}</td>
 
                         <td class="td" style="text-align: start">
-                            <p class="text-sm tooltip tooltip-top" data-tip="{{ $provider->provider->company_name }}">
-                                {{ auth()->user()->short($provider->provider->company_name, 15) }}
+                            <p class="text-sm tooltip tooltip-top" data-tip="{{ $providerName }}">
+                                {{ auth()->user()->short($providerName, 15) }}
                             </p>
                         </td>
 
                         <th class="th">
-                            {{ $provider->total }}
+                            {{ $total }}
                         </th>
 
                         <th class="td">
-                            {{ number_format(($provider->total / $totalStates) * 100, 2) }}%
+                            @if ($totalStates > 0)
+                                {{ number_format(($total / $totalStates) * 100, 2) }}%
+                            @else
+                                0%
+                            @endif
                         </th>
                     </tr>
                 @empty
                     <tr>
                         <x-utils.not-search message="No hay solicitudes finalizadas" colspan="4" py="py-20" />
                     </tr>
-                @endforelse --}}
+                @endforelse
             </tbody>
         </table>
     </div>
